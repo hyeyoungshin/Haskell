044c4e64b0f21f1b0af49a862edfb0e2554656ff
Hyeyoung Shin <hyeyoungshinw@gmail.com>
2014-12-30 22:44:42 -0500

    added more functions

--- baby.hs
+++ baby.hs
@@ -2,4 +2,16 @@
 
 doubleMe x = x + x
 
-doubleUs x y = x * 2 + y * 2
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber  = if x > 100
+                        then x  -- expression not statement
+                        else x*2 -- "else part is mandatory"
+
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+
+conanO'Brien = "It's a me, Conan O'Brien!"
+
+
+
+
