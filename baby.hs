7672be6b14d9275eca964c52183e240e34d9ceaa
Hyeyoung Shin <hyeyoungshinw@gmail.com>
2015-01-04 15:31:37 -0500

    to add functions with type declaration

--- baby.hs
+++ baby.hs
@@ -18,5 +18,14 @@ length' xs = sum [1 | _ <- xs]
 
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
 
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
+
 
 
